642445986bf70743a53e64e13ba5279cd895a7a5
sneha <snehanarayana1999@gmail.com>
2023-04-27 14:33:44 +0530

    Final

---
+++ js/vue-components/centerbar.vue
@@ -0,0 +1,223 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column is-3">
+                <aside class="menu is-hidden-mobile">
+                    <p class="menu-label">
+                        General
+                    </p>
+                    <ul class="menu-list">
+                        <li><a class="is-active">Dashboard</a></li>
+                        <li><a>Customers</a></li>
+                        <li><a>Other</a></li>
+                    </ul>
+                    <p class="menu-label">
+                        Starting Package
+                    </p>
+                    <div id="treeView"></div>
+                </aside>
+            </div>
+            <div class="column is-9">
+                <nav class="breadcrumb" aria-label="breadcrumbs">
+                    <ul>
+                        <li><a href="../">arith_add</a></li>
+                        <li><a href="../">125</a></li>
+                        <li><a href="../">genenric_tp1</a></li>
+                        <li class="is-active"><a href="#" aria-current="page">jobs</a></li>
+                    </ul>
+                </nav>
+                <section class="hero is-info welcome is-small">
+                    <div class="hero-body">
+                        <div class="container">
+                            <h1 class="title">
+                                arith_add 
+                            </h1>
+                            <h2 class="subtitle"> Variant : generic </h2> <h3>Date & Time:  3/23/2023, 17:16:13</h3>
+                        </div>
+                    </div>
+                </section>
+                <div class="box">
+                    <div class="columns" >
+                        <div class="column">
+                           <div class="row box" style="background-color: rgb(226, 225, 225);">
+                            <label style="color: darkblue; font-weight : bold">SP Commit : </label> <label style="font-weight : bold">master</label>
+                           </div>
+                           <div class="row box" style="background-color: rgb(226, 225, 225);">
+                            <label style="color: darkblue; font-weight : bold">Top Level Package : </label > <label style="font-weight : bold" >Caculator</label>
+                           </div>
+                    </div>
+                    <div class="column">
+                        <div class="row box" style="background-color: rgb(226, 225, 225);">
+                            <label style="color: darkblue; font-weight : bold">SP Platform : </label> <label style="font-weight : bold">Windows</label>
+                           </div>
+                           <div class="row box" style="background-color: rgb(226, 225, 225);">
+                            <label style="color: darkblue; font-weight : bold">Status : </label> <label style="font-weight : bold" >Success</label>
+                           </div>   
+                    </div>
+                    <div class="column">
+                        <div class="row box" style="background-color: rgb(226, 225, 225);">
+                            <label style="color: darkblue; font-weight : bold">Source : </label> <label style="font-weight : bold">Jenkins</label>
+                           </div>
+                           <div class="row box" style="background-color: rgb(226, 225, 225);">
+                            <label style="color: darkblue; font-weight : bold">Top Level Commit : </label> <label style="font-weight : bold">master</label>
+                           </div>    
+                    </div>
+                    </div>
+                    </div>
+                    <div class="columns box">
+                        <div class="column is-12">
+                            <div class="card events-card">
+                                <header class="card-header">
+                                    <p class="card-header-title">
+                                        Jobs - Build ID: 125
+                                    </p>
+                                    <a href="#" class="card-header-icon" aria-label="more options">
+                                    <span class="icon">
+                                        <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                                    </a>
+                                </header> <br>
+                                <div class="card-table is-fullheight">
+                                    <div class="content" style="height:100%">
+                                        <table id="table2" class="table is-fullwidth is-striped" style="width:100%; height: 100%;">
+                                            <thead>
+                                                <th>ID</th>
+                                                <th>Package</th>
+                                                <th>Major Version</th>
+                                                <th>Commit</th>
+                                                <th>Variant</th>
+                                                <th>Platform</th>
+                                                <th>Job Status</th>
+                                                <th>Date</th>
+                                                <th>Build Time</th>
+                                                <th>Jenkins URL</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1131</td>
+                                                    <td>arith_add</td>
+                                                    <td>2</td>
+                                                    <td>d7cedc539658be55ce22728afc35b067dd677c57</td>
+                                                    <td>generic</td>
+                                                    <td>vs2017</td>
+                                                    <td>Building</td>
+                                                    <td>7/29/2022, 04:15:35</td>
+                                                    <td>00:00:23</td>
+                                                    <td><a href="https://ci.adas-cip.conti.de/bdjm/job/bdjm-builds-0.13/job/job/3/">jenkins</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1131</td>
+                                                    <td>arith_add</td>
+                                                    <td>2</td>
+                                                    <td>d7cedc539658be55ce22728afc35b067dd677c57</td>
+                                                    <td>generic</td>
+                                                    <td>vs2017</td>
+                                                    <td>Building</td>
+                                                    <td>7/29/2022, 04:15:35</td>
+                                                    <td>00:00:23</td>
+                                                    <td><a href="https://ci.adas-cip.conti.de/bdjm/job/bdjm-builds-0.13/job/job/3/">jenkins</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1131</td>
+                                                    <td>arith_add</td>
+                                                    <td>2</td>
+                                                    <td>d7cedc539658be55ce22728afc35b067dd677c57</td>
+                                                    <td>generic</td>
+                                                    <td>vs2017</td>
+                                                    <td>Building</td>
+                                                    <td>7/29/2022, 04:15:35</td>
+                                                    <td>00:00:23</td>
+                                                    <td><a href="https://ci.adas-cip.conti.de/bdjm/job/bdjm-builds-0.13/job/job/3/">jenkins</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>1131</td>
+                                                    <td>arith_add</td>
+                                                    <td>2</td>
+                                                    <td>d7cedc539658be55ce22728afc35b067dd677c57</td>
+                                                    <td>generic</td>
+                                                    <td>vs2017</td>
+                                                    <td>Building</td>
+                                                    <td>7/29/2022, 04:15:35</td>
+                                                    <td>00:00:23</td>
+                                                    <td><a href="https://ci.adas-cip.conti.de/bdjm/job/bdjm-builds-0.13/job/job/3/">jenkins</a></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        </div>
+</template>
+
+<style scoped>
+.columns {
+    width: 100%;
+    height: 100%;
+    margin-left: 0;
+  
+  }
+  .menu-label {
+    color: #8F99A3;
+    letter-spacing: 1.3;
+    font-weight: 700;
+  }
+  .menu-list a {
+    color: #0F1D38;
+    font-size: 14px;
+    font-weight: 700;
+  }
+  .menu-list a:hover {
+    background-color: transparent;
+    color: #276cda;
+  }
+  .menu-list a.is-active {
+    background-color: transparent;
+    color: #276cda;
+    font-weight: 700;
+  }
+  .card {
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.18);
+    margin-bottom: 2rem;
+   
+  }
+  .card-header-title {
+    color: #8F99A3;
+    font-weight: 400;
+  }
+  .info-tiles {
+    margin: 1rem 0;
+  }
+  .info-tiles .subtitle {
+    font-weight: 300;
+    color: #8F99A3;
+  }
+  .hero.welcome.is-info {
+    background: #36D1DC;
+    background: -webkit-linear-gradient(to right, #5B86E5, #36D1DC);
+    background: linear-gradient(to right, #5B86E5, #36D1DC);
+  }
+  .hero.welcome .title, .hero.welcome .subtitle {
+    color: hsl(192, 17%, 99%);
+  }
+  .card .content {
+    font-size: 14px;
+  }
+  .card-footer-item {
+    font-size: 14px;
+    font-weight: 700;
+    color: #8F99A3;
+  }
+  .card-footer-item:hover {
+  }
+  .card-table .table {
+    margin-bottom: 0;
+  }
+  .events-card .card-table {
+    max-height: 250px;
+    overflow-y: scroll;
+  }
+</style>
---
+++ js/vue-components/navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar is-dark">
+      <div class="container">
+          <div class="navbar-brand">
+              <a class="navbar-item brand-text" href="../index.html">
+       BDJM
+      </a>
+              <div class="navbar-burger burger" data-target="navMenu">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+              </div>
+          </div>
+          <div id="navMenu" class="navbar-menu">
+              <div class="navbar-start">
+                  <a class="navbar-item" href="admin.html">
+          Home
+        </a>
+                  <a class="navbar-item" href="admin.html">
+          New Request
+        </a>
+                               <a class="navbar-item" href="admin.html">
+          Reports
+        </a>
+        <a class="navbar-item" href="admin.html">
+          Documentation
+        </a>
+              </div>
+
+          </div>
+      </div>
+  </nav>
+</template>
+<style scoped>
+html, body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+  height: 100%;
+  background: #ECF0F3;
+  
+}
+nav.navbar {
+   border-top: 4px solid #276cda;
+  margin-bottom: 1rem;
+  
+ 
+}
+
+.navbar-item.brand-text {
+  font-weight: 300;
+  
+}
+.navbar-item, .navbar-link {
+  font-size: 14px;
+  font-weight: 700;
+}
+</style>
